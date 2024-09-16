f3a1282204db6853469e8860f891b97fc66733d0
FlyingFool95 <jhernehult@gmail.com>
2024-09-16 19:14:57 +0200

    Setup routing + add router to main.tsx + add App.tsx as root route + add routes to Dashboard.tsx, Categories.tsx, Media.tsx, Favorites.tsx + add temp errorElement: <h1>404</h1>

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
+import { Outlet } from 'react-router-dom'
 import './styling/reset.scss'
 import './styling/global.scss'
 
-function App() {
-
+export default function App() {
     return (
         <div className='app'>
-            <h1>WEXO CC API MOVIES</h1>
-
+            <Outlet />
         </div>
     )
 }
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,44 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Categories from './routes/Categories'
+import Dashboard from './routes/Dashboard'
+import Favorites from './routes/Favorites'
+import Media from './routes/Media'
+import App from './App'
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <h1>404</h1>,
+        children: [
+            {
+                path: '/',
+                element: <Dashboard />,
+            },
+            {
+                path: '/categories',
+                element: <Categories />,
+            },
+            {
+                path: '/media/:mediaId',
+                element: <Media />,
+            },
+            {
+                path: '/favorites',
+                element: <Favorites />,
+            },
+        ],
+    },
+
+
+
+])
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </StrictMode>,
 )
