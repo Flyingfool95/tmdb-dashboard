b201ce864f4efc7d98fab909a3045dbbdf9da14a
FlyingFool95 <jhernehult@gmail.com>
2024-09-18 16:09:16 +0200

    Updating useFetch.ts + adding fetchGenres to fetch all the genres dynamically + commenting out the fetchDashboardData useQuery

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,8 +1,46 @@
-
 export default function useFetch() {
 
-const fetchDashboardData = async () => {
-    
-}
+    const TMDB_API_KEY = `?api_key=${import.meta.env.VITE_TMDB_API_KEY}`;
+    const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
+
+
+
+    const fetchGenres = async (type: string) => {
+        try {
+            const response = await fetch(`${TMDB_BASE_URL}/genre/${type}/list${TMDB_API_KEY}`);
+            const data = await response.json();
+            if (!data.genres) throw new Error(data.status_message)
+
+            console.log(data)
+            return data;
+        } catch (error) {
+            console.error(error)
+            return []
+        }
+    }
+
+    const fetchDashboardData = async () => {
+
+        try {
+            const response = await fetch(TMDB_BASE_URL + '/' + TMDB_API_KEY);
+
+            const data = await response.json();
+
+            if (!data.results) throw new Error(data.status_message)
+
+            console.log(data)
+            return data;
+
+        } catch (error) {
+            console.error(error)
+            return []
+        }
+
+    }
+
+    return {
+        fetchGenres,
+        fetchDashboardData,
+    }
 
 }
--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -1,4 +1,27 @@
+import useFetch from "../hooks/useFetch"
+import { useQuery } from "@tanstack/react-query"
+
+
 export default function Dashboard() {
+    const { fetchDashboardData, fetchGenres } = useFetch();
+
+    const movieGenres = useQuery({
+        queryKey: ['movie-genres'],
+        queryFn: () => fetchGenres("movie"),
+    })
+    const tvGenres = useQuery({
+        queryKey: ['tv-genres'],
+        queryFn: () => fetchGenres("tv"),
+    })
+
+    console.log(tvGenres.data)
+
+    /*     const { data, isLoading, error } = useQuery({ 
+            queryKey: ['dashboard'], 
+            queryFn: fetchDashboardData,
+            enabled: !!genres.data,
+        }) */
+
     return (
         <main className='dashboard'>
             <h1>Dashboard</h1>
