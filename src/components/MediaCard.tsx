95934e4eab193c7b760102527b1183d9fd399474
FlyingFool95 <jhernehult@gmail.com>
2024-09-20 14:07:12 +0200

    Adding no-poster-found.jpg image + fixing MediaCard.tsx flex-basis

--- src/components/MediaCard.tsx
+++ src/components/MediaCard.tsx
@@ -1,18 +1,28 @@
 import { Link } from 'react-router-dom';
 import useGlobalConstants from '../state/useGlobalConstants';
+import NO_POSTER_FOUND from '../assets/no-poster-found.jpg'
 
 export default function MediaCard({ media, mediaType }: { media: any, mediaType: string }) {
 
     const { TMDB_IMAGE_BASE_URL, TMDB_IMAGE_SIZE_THUMBNAIL } = useGlobalConstants()
 
 
+    if (media.poster_path === null) {
+        console.log(media)
+
+    }
+
     return (
         <Link to={`/${mediaType}/${media.id}`} className="media-card card-hover">
+
             <img
                 src={
-                    TMDB_IMAGE_BASE_URL +
-                    TMDB_IMAGE_SIZE_THUMBNAIL +
-                    media.poster_path
+                    media.poster_path ?
+                        (TMDB_IMAGE_BASE_URL +
+                            TMDB_IMAGE_SIZE_THUMBNAIL +
+                            media.poster_path)
+                        :
+                        NO_POSTER_FOUND
                 }
 
                 alt={`${media.title} poster`}
