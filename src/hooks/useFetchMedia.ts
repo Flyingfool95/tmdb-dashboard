5fb10a5d34cb465a9eb48ac989076a4f479c0d83
FlyingFool95 <jhernehult@gmail.com>
2024-09-23 12:12:30 +0200

    Changing order of const in useFetchMedia.ts

--- src/hooks/useFetchMedia.ts
+++ src/hooks/useFetchMedia.ts
@@ -4,8 +4,6 @@ import NO_IMAGE_FOUND from '../assets/no-poster-found.jpg';
 export default function useFetchMedia() {
     const { TMDB_API_KEY, TMDB_BASE_URL, TMDB_RESPONSE_LANG, TMDB_IMAGE_BASE_URL, TMDB_IMAGE_SIZE_BACKDROP, TMDB_IMAGE_SIZE_POSTER } = useGlobalConstants();
 
-
-
     const fetchMedia = async (mediaId: string, mediaType: string) => {
 
         if (!mediaType) return null;
@@ -21,9 +19,9 @@ export default function useFetchMedia() {
 
             const title = data.title ? data.title : data.name;
             const poster = data.poster_path ? TMDB_IMAGE_BASE_URL + TMDB_IMAGE_SIZE_POSTER + data.poster_path : NO_IMAGE_FOUND;
+            const backDrop = data.backdrop_path ? TMDB_IMAGE_BASE_URL + TMDB_IMAGE_SIZE_BACKDROP + data.backdrop_path : NO_IMAGE_FOUND;
             const releaseYear = data.release_date ? data.release_date.slice(0, 4) : data.first_air_date.slice(0, 4);
             const description = data.overview;
-            const backDrop = TMDB_IMAGE_BASE_URL + TMDB_IMAGE_SIZE_BACKDROP + data.backdrop_path;
             const genres = data.genres;
 
             return {
@@ -39,7 +37,6 @@ export default function useFetchMedia() {
             console.error(error);
             return null;
         }
-
     };
 
     return {
