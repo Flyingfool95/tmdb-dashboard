cd7c4ce6ed8babebab4fc300eacab896a564151c
FlyingFool95 <jhernehult@gmail.com>
2024-09-23 12:05:46 +0200

    refactored Media.tsx and useFetchMedia.ts + reforated data returned from useFetchMEdia.ts + cleaned up Media.tsx

--- src/hooks/useFetchMedia.ts
+++ src/hooks/useFetchMedia.ts
@@ -1,13 +1,15 @@
 import useGlobalConstants from "../state/useGlobalConstants";
+import NO_IMAGE_FOUND from '../assets/no-poster-found.jpg';
 
 export default function useFetchMedia() {
-    const { TMDB_API_KEY, TMDB_BASE_URL, TMDB_RESPONSE_LANG } = useGlobalConstants();
+    const { TMDB_API_KEY, TMDB_BASE_URL, TMDB_RESPONSE_LANG, TMDB_IMAGE_BASE_URL, TMDB_IMAGE_SIZE_BACKDROP, TMDB_IMAGE_SIZE_POSTER } = useGlobalConstants();
+
 
 
     const fetchMedia = async (mediaId: string, mediaType: string) => {
 
         if (!mediaType) return null;
-        
+
         try {
             const type = mediaType === 'movies' ? 'movie' : 'tv';
 
@@ -17,8 +19,22 @@ export default function useFetchMedia() {
 
             if (!data) throw new Error(data.status_message);
 
-            console.log(data)
-            return data;
+            const title = data.title ? data.title : data.name;
+            const poster = data.poster_path ? TMDB_IMAGE_BASE_URL + TMDB_IMAGE_SIZE_POSTER + data.poster_path : NO_IMAGE_FOUND;
+            const releaseYear = data.release_date ? data.release_date.slice(0, 4) : data.first_air_date.slice(0, 4);
+            const description = data.overview;
+            const backDrop = TMDB_IMAGE_BASE_URL + TMDB_IMAGE_SIZE_BACKDROP + data.backdrop_path;
+            const genres = data.genres;
+
+            return {
+                id: data.id,
+                title,
+                poster,
+                releaseYear,
+                description,
+                backDrop,
+                genres,
+            };
         } catch (error) {
             console.error(error);
         }
--- src/routes/Media.tsx
+++ src/routes/Media.tsx
@@ -1,17 +1,13 @@
+import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import useFetchMedia from "../hooks/useFetchMedia";
 import usePathname from "../hooks/usePathname";
-import { useParams } from "react-router-dom";
-import useGlobalConstants from "../state/useGlobalConstants";
 
 export default function Media() {
 
     const { mediaId } = useParams();
     const { mediaType } = usePathname();
     const { fetchMedia } = useFetchMedia();
-
-    const { TMDB_IMAGE_BASE_URL, TMDB_IMAGE_SIZE_BACKDROP, TMDB_IMAGE_SIZE_POSTER } = useGlobalConstants()
-
 
     if (!mediaId) return null;
 
@@ -24,36 +20,29 @@ export default function Media() {
 
     if (!mediaData.data) return null;
 
-    const title = mediaData.data.title ?? mediaData.data.name;
-    const poster = TMDB_IMAGE_BASE_URL + TMDB_IMAGE_SIZE_POSTER + mediaData.data.poster_path;
-    const releaseYear = mediaData.data.release_date.slice(0, 4) ?? mediaData.data.first_air_date.slice(0, 4);
-    const description = mediaData.data.overview;
-    const backDrop = TMDB_IMAGE_BASE_URL + TMDB_IMAGE_SIZE_BACKDROP + mediaData.data.backdrop_path;
-    const genres = mediaData.data.genres
-
     return (
 
-        <main className="media" style={{ backgroundImage: `url(${backDrop})` }
+        <main className="media" style={{ backgroundImage: `url(${mediaData.data.backDrop})` }
         }>
 
             <div className="media__poster">
-                <img src={poster} alt={title} />
+                <img src={mediaData.data.poster} alt={mediaData.data.title} />
             </div>
 
             <div className="media__details">
-                <h1>{title}</h1>
+                <h1>{mediaData.data.title}</h1>
 
-                <p>{releaseYear}</p>
+                <p>{mediaData.data.releaseYear}</p>
 
                 <ul className="media__genres">
                     {
-                        genres && genres.map((genre: any) => (
+                        mediaData.data.genres && mediaData.data.genres.map((genre: any) => (
                             <li key={genre.id}>{genre.name}</li>
                         ))
                     }
                 </ul>
 
-                <p>{description}</p>
+                <p>{mediaData.data.description}</p>
             </div>
         </main>
 
